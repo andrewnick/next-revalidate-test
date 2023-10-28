3b5ad0c656c337ce24109c627de0ab7304596a67
Andrew Nicholas <andrew@vyg.co.nz>
2023-10-28 13:13:42 +1300

    add logging

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,6 +1,13 @@
 import { revalidateTag } from "next/cache";
+import { cookies } from "next/headers";
 
 export async function GET() {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token");
+
+  console.log({ token });
+  console.log("revalidating");
+
   revalidateTag("navigation");
 
   return Response.json({ message: "Success" });
